ba99a27b45c81ae642ae8d4bec4ddd274ebb84c0
pichaachuu <nafisahaliyah01@gmail.com>
2024-11-22 18:38:38 +0700

    Jobsheet 10

--- Bioskop13.java
+++ Bioskop13.java
@@ -16,5 +16,49 @@ public class Bioskop13 {
         System.out.printf("%s \t %s\n", penonton[2][0], penonton [2][1]);
         System.out.printf("%s \t %s\n", penonton[3][0], penonton [3][1]);
 
+        //pertanyaan 4
+        System.out.println("\n===Pertanyaan 4===");
+        System.out.println(penonton.length);
+        System.out.println(penonton[0].length);
+        System.out.println(penonton[1].length);
+        System.out.println(penonton[2].length);
+        System.out.println(penonton[3].length);
+
+        //pertanyaan 5
+        System.out.println("\n===Modifikasi Pertanyaan 4 Memakai For Loop===");
+        System.out.println(penonton.length);
+
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Panjang baris ke-" + (i+1) + ": " + penonton[i].length);
+        }
+
+        //pertanyaan 6
+        System.out.println("\n===Modifikasi Pertanyaan 4 Memakai Foreach===");
+        System.out.println(penonton.length);
+        for (String[] barisPenonton : penonton) {
+            System.out.println("Panjang baris: " +barisPenonton.length);
+        }
+
+        //pertanyaan 7
+        System.out.println("\nPenonton pada baris ke-3: ");
+
+        for (int i = 0; i < penonton[2].length; i++) {
+            System.out.println(penonton[2][i]);
+        }
+        
+
+        //pertanyaan 8
+        System.out.println("\n===Modifikasi Pertanyaan 7 (foreach)");
+        System.out.println("Penonton pada baris ke-3: ");
+
+        for (String i : penonton[2]) {
+            System.out.println(i);
+        }
+
+        //pertanyaan 9
+        System.out.println("\n===Pertanyaan 9===");
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Penonton pada baris ke-" + (i+1) + ": " + String.join(", ", penonton[i]));
+        }
     }
 }
